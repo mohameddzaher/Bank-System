1c215b85b97f43a0f42930d636c70933f7dd6488
mohamed zaher <mohamedzaher.dev@gmail.com>
2022-09-19 18:24:33 +0200

    Initial Commit

---
+++ Bank System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bank System
+//
+//  Created by Mohamed Zaher on 19/09/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
